174c11577c072c18682125fe45cca8dd932ff481
sash <sash2104@gmail.com>
2016-11-23 17:21:22 +0900

    [MOD] change response randomly

--- mod_carcassonne.cpp
+++ mod_carcassonne.cpp
@@ -11,6 +11,7 @@
 #include "http_protocol.h"
 #include <iostream>
 #include <string>
+#include <sstream>
 
 extern "C" module AP_MODULE_DECLARE_DATA carcassonne_module;
 
@@ -24,13 +25,14 @@ typedef struct {
 
 static void *create_dir_config(apr_pool_t *p, char *path)
 {
+    // srand((unsigned) time(NULL));
     carcassonne_dir_config *cfg;
     try {
       cfg = new carcassonne_dir_config;
     } catch(...) {
       exit(1);
     }
-    cfg->message = "{\"tile\":[1,0,0,0,0],\"next_tile\":2}";
+    // cfg->message = "{\"tile\":[1,0,0,0,0],\"next_tile\":2}";
     return (void *) cfg;
 }
 
@@ -43,11 +45,15 @@ static int carcassonne_handler(request_rec *r)
     if (!r->handler || strcmp(r->handler, "carcassonne")) return (DECLINED);
     carcassonne_dir_config *cfg = (carcassonne_dir_config *)ap_get_module_config(r->per_dir_config, &carcassonne_module);
 
+    std::ostringstream ss;
+    ss << "{\"tile\":[" << rand()%4 << "," << rand()%4 << "," << rand()%4 << ",0,0],\"next_tile\":2}";
+
     /* Now that we are handling this request, we'll write out "Hello, world!" to the client.
      * To do so, we must first set the appropriate content type, followed by our output.
      */
     ap_set_content_type(r, "application/json");
-    ap_rprintf(r, cfg->message.c_str());
+    // ap_rprintf(r, cfg->message.c_str());
+    ap_rprintf(r, ss.str().c_str());
 
     /* Lastly, we must tell the server that we took care of this request and everything went fine.
      * We do so by simply returning the value OK to the server.
